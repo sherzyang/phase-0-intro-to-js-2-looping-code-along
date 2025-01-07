089dc092f3ec0aabffd1d527f6288856cabf71d8
Sherry Yang <19577448+sherzyang@users.noreply.github.com>
2025-01-06 17:32:20 -0800

    Update.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+
+let names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, occassion = "birthday") {
+    let newCards = []
+    for ( let i =0; i < names.length; i++) {
+        newCards.push(`Thank you, ${names[i]}, for the wonderful ${occassion} gift!`);
+    }
+    return newCards;
+}
+
+writeCards(names, occassion);
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
